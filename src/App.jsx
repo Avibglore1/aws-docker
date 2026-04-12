4354f223b7943ee38b7e312abaefbd3222bb7a40
Avinash <avinashkumarofficial95@gmail.com>
2026-04-12 15:01:13 +0530

    installed tailwind ND REACT

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex justify-center items-center'>
+    <h1 className='text-red-500 font-bold text-center '>Hey</h1>
+    </div>
+  )
+}
+
+export default App
