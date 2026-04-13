d5430fdccde68205157b4d47da1fadd77c8057f1
Avinash <avinashkumarofficial95@gmail.com>
2026-04-13 17:39:41 +0530

    created a editor for on-time dynamic change across all users

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./App.css"
+import "./app/app.css"
 import './index.css'
-import App from './App.jsx'
+import App from './app/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
